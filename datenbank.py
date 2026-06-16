072a29a44b3693932f92800d5c769a53e5487c39
lucymahn-creator <lucy.mahn@outlook.com>
2026-06-16 11:01:04 +0200

    Update datenbank.py

--- datenbank.py
+++ datenbank.py
@@ -3,7 +3,15 @@ import pandas as pd
 import io
 
 REMOTE_PATH = "/Finanz-App/datenbank.csv"
-https://github.com/lucymahn-creator/FinanzApp/blob/main/datenbank.py
+
+def get_client(user, password):
+    return easywebdav2.connect(
+        "cloud.zagorko.com",
+        username=user,
+        password=password,
+        protocol="https"
+    )
+
 def lade_eintraege(user, password, bereich=None):
     client = get_client(user, password)
     buffer = io.BytesIO()
@@ -16,13 +24,17 @@ def lade_eintraege(user, password, bereich=None):
 
 def speichere_eintrag(user, password, ber, typ, kat, betrag, dat, zus=""):
     client = get_client(user, password)
-    # ... (Rest wie gehabt, verwende 'client' hier)
+    # Lade aktuelle Daten
+    buffer = io.BytesIO()
+    client.download(REMOTE_PATH, buffer)
+    buffer.seek(0)
+    df = pd.read_csv(buffer)
     
-    # 2. Neue Zeile anhängen
+    # Neue Zeile anhängen
     neue_zeile = {"ID": "", "Bereich": ber, "Typ": typ, "Kategorie": kat, "Betrag": betrag, "Datum": dat, "Zusatz": zus}
     df = pd.concat([df, pd.DataFrame([neue_zeile])], ignore_index=True)
     
-    # 3. Datei zurückschreiben
+    # Hochladen
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer, index=False)
     client.upload(csv_buffer.getvalue(), REMOTE_PATH)
